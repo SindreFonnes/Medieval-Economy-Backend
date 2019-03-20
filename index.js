62b1f7f46debe0ee291d403db567f2a9c8ebb640
SindreFonnes <sindre.fonnes@gmail.com>
2019-03-20 18:18:34 +0100

    cleaned up file, added log, starting on end turn

--- index.js
+++ index.js
@@ -5,110 +5,137 @@ const path = require('path');
 const wss = new WebSocket.Server({ port: 8888 });
 
 let data = {}
+let log = []
 
-
-
-
-function getDate(){
+function getDate() {
     let today = new Date();
     let dd = today.getDate();
-    let mm = today.getMonth()+1; //January is 0!
+    let mm = today.getMonth() + 1; //January is 0!
     let yyyy = today.getFullYear();
 
-    if(dd<10) {
-        dd = '0'+dd
-    } 
+    if (dd < 10) {
+        dd = '0' + dd
+    }
 
-    if(mm<10) {
-        mm = '0'+mm
-    } 
+    if (mm < 10) {
+        mm = '0' + mm
+    }
 
     return today = dd + '_' + mm + '_' + yyyy;
 }
 
 let today = getDate()
 
-function saveToDataFile(func){
+function saveToDataFile(func) {
     fs.writeFile('data.JSON', JSON.stringify(data), 'utf8', (err) => {
         console.log(err)
         func();
     })
-
+    fs.writeFile('log.JSON', JSON.stringify(log), 'utf8', (err) => {
+        console.log(err)
+        func();
+    })
 }
 
-
-function readDataFile(func){
-    fs.readFile('data.json', 'utf8', function readFileCallback(err, content){
-        console.log('reading file');
-        if(err){
+function readDataFile(func) {
+    console.log('reading file');
+    fs.readFile('data.json', 'utf8', function readFileCallback(err, content) {
+        if (err) {
             console.log(err);
         } else {
             data = JSON.parse(content);
             func();
         }
     })
-}
-
-
-function makeBackupFile(func){
-    fs.writeFile(path.resolve(__dirname,"./backup/",today+"_backup.json"), JSON.stringify(data), function (err) {
-        if(err) throw err;
-        console.log('Saved!');
-        func();
+    fs.readFile('log.json', 'utf8', function readFileCallback(err, content) {
+        if (err) {
+            console.log(err);
+        } else {
+            log = JSON.parse(content);
+            func();
+        }
     })
 }
 
+function makeBackupFile(func) {
+    fs.writeFile(path.resolve(__dirname, "./backup/", today + "_backup.json"), JSON.stringify(data), function (err) {
+        if (err) throw err;
+        func();
+    })
+    fs.writeFile(path.resolve(__dirname, "./backup/", today + "_log.json"), JSON.stringify(log), function (err) {
+        if (err) throw err;
+        
+        func();
+    })
+    console.log('Saved!');
+}
 
 wss.on('connection', function connection(ws) {
 
-    function sendData(message){
+    function sendData(message) {
         ws.send(JSON.stringify(message))
     }
 
     ws.on('message', function incoming(text) {
-  
-        let message = JSON.parse(text)
-        if (message.type === 0){
+        let message;
+        try {
+            message = JSON.parse(text)
+        } catch (err) {
+            console.log(err)
+        }
+        
+        if (message.type === 0) {
             console.log(message.content)
         }
-        if(message.type === 1){
+
+        if (message.type === 1) {
             //end turn
             data = message.content
             console.log("Data recived!")
         }
-        if(message.type === 2){
+
+        if (message.type === 2) {
             //data sync
-            data = message.content            
+            data = message.content
             saveToDataFile(() => {
                 console.log("Data synced!")
             })
         }
-        if(message.type === 3){
+
+        if (message.type === 3) {
             //fetch data
             readDataFile(() => {
                 let response = {
                     type: 0,
-                    content: data
+                    content: [
+                        data,
+                        log
+                    ]
                 }
                 sendData(response)
                 console.log('Save data sent!')
             });
         }
-        if(message.type === 4){
+
+        if (message.type === 4) {
             makeBackupFile(() => {
                 console.log('Backupfile made!')
             })
         }
-        
-  });
+    });
 
-
-  let init = {
-      type: 10,
-      content: "Connection confirmed"
-  }
-  sendData(init);
+    readDataFile(() => {
+        let response = {
+            type: 0,
+            content: [
+                data,
+                log
+            ]
+        }
+        sendData(response)
+        console.log('Save data sent!')
+    });
 });
 
 console.log('Server is listening at port: 8888')
-readDataFile(() => {})
+readDataFile(() => { })
